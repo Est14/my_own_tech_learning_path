868cb10ff7c34723bd42603ae6c8978b902f2912
Esteban Martinez <esteban.tvp@gmail.com>
2020-12-24 11:40:55 -0500

    includes str js

--- 6.0-javascript/0.0-fundamentals/3.0-str.js
+++ 6.0-javascript/0.0-fundamentals/3.0-str.js
@@ -1,10 +1,17 @@
-var name = 'Esteban';
-var lastName = 'martinez'
+var name1 = "Esteban";
+var lastname1 = "martinez";
 
-var nameInUpperCase = name.toUpperCase();  // toUpperCase is a funtion
-var nameInLowerCase = name.toLowerCase();  // toLowerCase is a funtion
-var lenOfName = name.length  // length is an atribute 
-var fullName = `${name} ${lastName}`;
-var str = name.substr(1,2); // str is a funtion
-var capitalaizeLastName = lastName[0].toUpperCase() + lastName.substr(1, lastName.length - 1)
-var capitalaizeLastName = lastName.replace(lastName[0], (l) => l.toUpperCase())
+let str1 = new String("A object string");
+console.log(typeof lastname1);
+console.log(str1.length);
+
+var name1InUpperCase = name1.toUpperCase(); // toUpperCase is a funtion
+var name1InLowerCase = name1.toLowerCase(); // toLowerCase is a funtion
+var lenOfname1 = name1.length; // length is an atribute
+var fullname1 = `${name1} ${lastname1}`;
+var str = name1.substr(1, 2); //
+var capitalaizeLastname1 =
+  lastname1[0].toUpperCase() + lastname1.substr(1, lastname1.length - 1);
+var capitalaizeLastname1 = lastname1.replace(lastname1[0], (l) =>
+  l.toUpperCase()
+);
