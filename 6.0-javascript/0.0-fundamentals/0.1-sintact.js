bf862a17a03b96f92412fe6d40c3dc2633917176
Esteban Martinez <esteban.tvp@gmail.com>
2020-12-24 10:04:25 -0500

    js sintac

---
+++ 6.0-javascript/0.0-fundamentals/0.1-sintact.js
@@ -0,0 +1,41 @@
+// Snake case for files
+// mi_archivo_javascript.js;
+
+// UPPER_CASE in constants
+const UNA_CONSTANTE = "Soy una constante";
+
+// UpperCamelCase in clases
+class SerHumano {
+    constructor(nombre, genero) {
+      this.nombre = nombre;
+      this.genero = genero;
+    }
+  
+    miNombreEs() {
+      return `Mi nombre es ${this.nombre}`;
+    }
+  }
+
+// lowerCamelCase in varibles, funtions
+const unObjeto = {
+    nombre: "Jonathan",
+    email: "jonmircha@gmail.com",
+  };
+
+function holaMundo(nombre) {
+    alert(`Hola mundo ${nombre}`);
+  }
+
+holaMundo("Jonathan");
+
+// Ordenamiento de código
+
+// 1. IMPORTACIÓN DE MÓDULOS.
+// 2. DECLARACIÓN DE VARIABLES.
+// 3. DECLARACIÓN DE FUNCIONES.
+// 4. EJECUCIÓN DE CÓDIGO.
+
+
+
+
+
