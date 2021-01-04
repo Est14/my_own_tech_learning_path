fb2ff623a9800536fbfd46c73d4af15f120eef15
Esteban Martinez <esteban.tvp@gmail.com>
2021-01-04 10:18:31 -0500

    ok

---
+++ 8.0-Java/src/Strings.java
@@ -0,0 +1,12 @@
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Esteban";
+
+        System.out.println("This is my name: " + name);
+        System.out.println("My name has " + name.length() + " words");
+        System.out.println("The first letter of my name is: " + name.charAt(0));
+        System.out.println("The last letter of my name is: " + name.charAt(name.length() - 1));
+        
+    }
+}
