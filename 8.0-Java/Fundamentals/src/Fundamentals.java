8d38ffcb03d4202037430911754c784f4f51a8af
Esteban Martinez <esteban.tvp@gmail.com>
2021-01-06 08:40:49 -0500

    update

---
+++ 8.0-Java/Fundamentals/src/Fundamentals.java
@@ -0,0 +1,11 @@
+
+public class Fundamentals {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Esteban this is a task");
+
+        var num = 1;
+    }
+    
+}
