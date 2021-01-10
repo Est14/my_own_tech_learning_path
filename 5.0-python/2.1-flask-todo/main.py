bb6e42efc5d95b213ac6eaf8ea588b2c6b645f3e
Esteban Martinez <esteban.tvop@gmail.com>
2021-01-10 18:52:46 -0500

    all

--- 5.0-python/2.1-flask-todo/main.py
+++ 5.0-python/2.1-flask-todo/main.py
@@ -1,15 +1,41 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, session, redirect, url_for
+from flask import flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import CsrfProtect
+from forms import Login
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'SiulNabetse'
+csrf = CsrfProtect(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = Login()
+
+    if login_form.validate_on_submit():
+        # This save the user name in a session
+        session['username'] = login_form.username.data
+
+        flash('Session exitosa')
+        return redirect(url_for('profile'))
+    else:
+        return render_template('login.html',
+                               login_form=login_form)
+
+
+@app.route('/profile')
+def profile():
+    username = session['username']
+    return render_template('profile.html', username=username)
 
 
 @app.errorhandler(404)
