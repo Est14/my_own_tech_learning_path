3917a6fd7ef3dcda65729c4b031b2dc6485be329
Esteban Martinez <esteban.tvop@gmail.com>
2021-01-10 06:54:35 -0500

    adgrade

--- 5.0-python/2.0-flask/main.py
+++ 5.0-python/2.0-flask/main.py
@@ -1,37 +1,20 @@
-from flask import Flask, request,
-from flask import redirect, make_response, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
 
 
 app = Flask(__name__)
 
-todos = ['TODO 1', 'TODO 2', 'TODO 3']
-
 
 @app.route('/')
 def index():
-    # Esto es igual a la IP del usuario
-    user_ip = request.remote_addr
-
-    # Esto es la respuesta del servidor
-    response = make_response(redirect('/hello'))
-
-    # Esto crea una cookie con el valor de la IP del usuario
-    # La cual llamaremos userip1
-    response.set_cookie('user_ip1', user_ip)
-
-    return response
-
-# Este es un decorador de flask para establecer rutas
+    return render_template('index.html')
 
 
 @app.route('/hello')
 def hello():
+    pass
 
-    user_ip2 = request.cookies.get('user_ip1')
 
-    context = {
-        'user_IP': user_ip2,
-        'todos': todos
-    }
-
-    return render_template('hello.html', **context)
+if __name__ == "__main__":
+    app.run(debug=True)
