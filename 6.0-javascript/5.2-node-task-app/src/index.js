674869927d396a3c95c3c4eca621a8f17534162f
Esteban Martinez <esteban.tvop@gmail.com>
2021-01-19 18:17:56 -0500

    instal nodemon

---
+++ 6.0-javascript/5.2-node-task-app/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+
+// Initializations 
+const app = express()
+
+
+// Settings 
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs ({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: ,
+    extname:
+}))
+
+// Middleware
+
+
+
+// Gloval varibles
+
+
+// Routes 
+
+
+
+// Static files 
+
+
+// Server run
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+})
