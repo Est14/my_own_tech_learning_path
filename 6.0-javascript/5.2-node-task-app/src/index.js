3df0a512d51da9dc58d4e3b0aad0ab6dfb9d00c4
Esteban Martinez <esteban.tvop@gmail.com>
2021-01-20 19:45:49 -0500

    update js

--- 6.0-javascript/5.2-node-task-app/src/index.js
+++ 6.0-javascript/5.2-node-task-app/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
-
+const methodOverride = require('method-override')
+const session = require('express-session')
 
 // Initializations 
 const app = express()
@@ -13,19 +14,28 @@ app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', exphbs ({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: ,
-    extname:
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
 }))
+app.set('view engine', '.hbs')
 
 // Middleware
-
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.use(session({
+    secret: 'SiulNabetse',
+    resave: true,
+    saveUninitialized: true
+}))
 
 
 // Gloval varibles
 
 
 // Routes 
-
+app.use(require('./routes/index'))
+app.use(require('./routes/notes'))
+app.use(require('./routes/users'))
 
 
 // Static files 
@@ -34,4 +44,4 @@ app.engine('.hbs', exphbs ({
 // Server run
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`)
-})
+})  
