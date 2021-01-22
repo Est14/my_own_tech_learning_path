2e24f4e8538116de35d2ac12e9cbc50058458e7c
Esteban Martinez <esteban.tvop@gmail.com>
2021-01-22 11:27:22 -0500

    create crud

--- 6.0-javascript/5.2-node-task-app/src/index.js
+++ 6.0-javascript/5.2-node-task-app/src/index.js
@@ -1,49 +1,49 @@
-const express = require('express')
-const path = require('path')
-const exphbs = require('express-handlebars')
-const methodOverride = require('method-override')
-const session = require('express-session')
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
 
-// Initializations 
-const app = express()
-const db = require('./dabases')
+// Initializations
+const app = express();
+const db = require("./dabases");
 
-
-// Settings 
-app.set('port', process.env.PORT || 3500)
-app.set('views', path.join(__dirname, 'views'))
-app.engine('.hbs', exphbs ({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
-}))
-app.set('view engine', '.hbs')
-
+// Settings
+app.set("port", process.env.PORT || 3500);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
 
 // Middleware
-app.use(express.urlencoded({extended: false}))
-app.use(methodOverride('_method'))
-app.use(session({
-    secret: 'SiulNabetse',
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "SiulNabetse",
     resave: true,
-    saveUninitialized: true
-}))
-
+    saveUninitialized: true,
+  })
+);
 
 // Gloval varibles
 
+// Routes
+app.use(require("./routes/index"));
+app.use(require("./routes/notes"));
+app.use(require("./routes/users"));
 
-// Routes 
-app.use(require('./routes/index'))
-app.use(require('./routes/notes'))
-app.use(require('./routes/users'))
-
-
-// Static files 
-app.use(express.static(path.join(__dirname, 'public')))
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // Server run
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`)
-})  
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
+});
--- 6.0-javascript/5.2-node-task-app/src/routes/notes.js
+++ 6.0-javascript/5.2-node-task-app/src/routes/notes.js
@@ -1,32 +1,53 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
+const Note = require("../models/Note");
 
-router.get('/notes/add', (req, res) => {
-    res.render('notes/new-notes')
-})
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-notes");
+});
 
-router.post('/notes/new-notes', (req, res) => {
-    const {title, description} = req.body
-    const errors = []
-    if(!title){
-        errors.push({text: 'Please write a Title'})
-    }
-    if (!description){
-        errors.push({text:'Please write an Description'})
-    }
-    if (errors.length > 0){
-        res.render('notes/new-notes', {
-            errors,
-            title,
-            description
-        })
-    }else{
-        res.send('ok')
-    }
-})
+router.post("/notes/new-notes", async (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+  if (!title) {
+    errors.push({ text: "Please write a Title" });
+  }
+  if (!description) {
+    errors.push({ text: "Please write an Description" });
+  }
+  if (errors.length > 0) {
+    res.render("notes/new-notes", {
+      errors,
+      title,
+      description,
+    });
+  } else {
+    const newNote = new Note( {title, description})
+    await newNote.save()
+    res.redirect('/notes')
+  }
+});
 
-router.get('/notes', (req, res) => {
-    res.render('notes/new-notes')
-})
+router.get("/notes", async (req, res) => {
+  const notes = await Note.find({}).lean().sort({date: 'desc'});
+  res.render('notes/all-notes', {notes})
+});
 module.exports = router;
+
+router.get("/notes/edit/:id", async (req, res) => {
+    const note = await Note.findById(req.params.id).lean()
+    res.render('notes/edit-note', {note})
+})
+
+router.put("/notes/edit-note/:id", async (req, res) => {
+    const {title, description} = req.body
+    console.log(`Esto es el contenido: ${title} ${description}`);
+    await Note.findByIdAndUpdate(req.params.id, {title, description}).lean()
+    res.redirect('/notes')
+})
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id)
+    res.redirect('/notes')
+})
