a6477235c283d15bedfe1ea5d36b67fc8f6aa2f8
Esteban Martinez <esteban.tvp@gmail.com>
2021-01-28 17:02:22 -0500

    dom

---
+++ 6.0-javascript/3.0-dom/index.js
@@ -0,0 +1,8 @@
+// Select by Id
+let  paragraph =  document.getElementById('paragraph')
+document.write(paragraph)
+
+// Select by class
+const range = document.querySelector(".range")
+// Modifique attribute
+range.setAttribute('type', 'color')
